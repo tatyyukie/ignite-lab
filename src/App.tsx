e228a69fa447d913c6d0cf13827ad828acacad6a
tatyyukie <tatyy-yukie@hotmail.com>
2022-10-13 10:24:27 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+export function App(){
+  return (
+    <> 
+      <h1 className="font-bold text-2xle text-violet-500">Batata Batata Batata</h1>
+
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">
+        Enviar
+      </button>
+    </>
+  )
+}
